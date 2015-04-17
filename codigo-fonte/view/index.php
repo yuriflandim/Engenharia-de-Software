4816581677082b92dbdd7790b3b0719bee71873f
Carlos Henrique <microheyn@gmail.com>
2015-04-16 21:40:27 -0300

    Página de login

--- codigo-fonte/view/index.php
+++ codigo-fonte/view/index.php
@@ -1,7 +1,8 @@
 <?php
 
-    include_once("./header.html");
-    include_once("./cadastrar-operador.html");
-    include_once("./footer.html");
+    include_once("./login.html");
+//    include_once("./header.html");
+//    include_once("./cadastrar-operador.html");
+//    include_once("./footer.html");
 
 ?>
